21a8cdda2f278ee60db722dd5e797088d2d98b1c
Carlos Florêncio <carlosmflorencio@gmail.com>
2023-10-24 20:54:18 +0100

    stage 2

--- app/main.js
+++ app/main.js
@@ -1,11 +1,18 @@
 const net = require("net");
 
-
 const server = net.createServer((socket) => {
   socket.on("close", () => {
     socket.end();
     server.close();
   });
+
+  socket.on("data", () => {
+    socket.end("HTTP/1.1 200 OK\r\n\r\n")
+  })
 });
 
 server.listen(4221, "localhost");
+
+server.on("error", err => {
+  console.error(err)
+})
